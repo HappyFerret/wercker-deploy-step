22b794f069372aae04ba369a953f231ea5e45b9e
richrocksmy <rich.jeffrey@localheroes.com>
2017-08-01 11:30:14 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,34 @@
+set -e
+
+# Environment variables
+REPO="$TERRAFORM_REPOSITORY_NAME"
+USER="$GITHUB_USERNAME"
+USER_EMAIL="$GITHUB_EMAIL"
+SERVICE="$SERVICE_TO_DEPLOY"
+ENVIRONMENT="$TARGET_ENVIRONMENT"
+COMMIT_HASH="$WERCKER_UPDATE_TERRAFORM_COMMIT_HASH"
+
+URL="https://$USER:$API_TOKEN@github.com/$GITHUB_ACCOUNT/$REPO.git"
+
+git clone $URL $REPO
+cd ./$REPO/_support
+git pull
+
+######
+#Temporary step so we don't push directly to master
+git checkout feat/lh-799-auto-deploy-update
+git pull
+######
+
+yarn
+node deploy.js $SERVICE $ENVIRONMENT $COMMIT_HASH
+
+git config user.name $USER
+git config user.email $USER_EMAIL
+git add ../$ENVIRONMENT/service-versions.tf
+
+git commit -m "Deploying $SERVICE to $ENVIRONMENT ($COMMIT_HASH)" 
+git push "https://$USER@github.com/$GITHUB_ACCOUNT/$REPO.git"
+
+cd ../../
+rm -rf $REPO
