15fd23c80ec6fa810d716ed0551c6e551e59879d
Antoine Savignac <antoine.savignac@mylocalheroes.com>
2020-05-13 15:25:29 +0100

    Update build and deploy script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 docker build -t deploy-terraform .
-aws ecr get-login --no-include-email --region eu-west-1 --profile lh_global | /bin/sh
-docker tag deploy-terraform:latest 611461148299.dkr.ecr.eu-west-1.amazonaws.com/wercker-base-images:deploy-terraform
-docker push 611461148299.dkr.ecr.eu-west-1.amazonaws.com/wercker-base-images:deploy-terraform
+#aws ecr get-login --no-include-email --region eu-west-1 --profile lh_global | /bin/sh
+aws ecr get-login-password --region eu-west-1 --profile ecr_admin | docker login --username AWS --password-stdin 611461148299.dkr.ecr.eu-west-1.amazonaws.com
+docker tag deploy-terraform:latest 611461148299.dkr.ecr.eu-west-1.amazonaws.com/base-images:deploy-terraform
+docker push 611461148299.dkr.ecr.eu-west-1.amazonaws.com/base-images:deploy-terraform
